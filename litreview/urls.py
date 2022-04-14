a8350ac78691e06d52eede8565e5afb2c7b41977
Wil31 <80322222+Wil31@users.noreply.github.com>
2022-04-14 16:25:09 +0200

    Added user registration page

--- litreview/urls.py
+++ litreview/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('change-password-done/', PasswordChangeDoneView.as_view(
         template_name='authentication/password_change_done.html'),
         name='password_change_done'),
+    path('signup/', authentication.views.signup_page, name='signup'),
 ]
