ddf368e98877581be649fffb7dc67ef10a57755a
Wil31 <80322222+Wil31@users.noreply.github.com>
2022-04-15 17:23:19 +0200

    Add Ticket model to Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    descritpion = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
